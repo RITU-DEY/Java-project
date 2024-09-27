95cec218ba516c5159c19cfe707530a1f785840d
RITU-DEY <dritu2172@gmail.com>
2024-09-27 14:23:33 +0600

    Add files via upload

---
+++ Interfaces/IloginOperation.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface IloginOperation {
+    void matchAdmin(String textField1, String textField2);
+
+    void matchUser(String textField1, String textField2);
+}
---
+++ Interfaces/iRegistrationOperation.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface iRegistrationOperation {
+    void registration(String textField5, String textField6, String textField4, String textField8, String secQsna);
+}
